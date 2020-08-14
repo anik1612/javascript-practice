99716115242a56c3922a4cbb2b3a660de5d35e72
Anik Sarker <aniksarker1612@gmail.com>
2020-08-15 02:45:15 +0800

    destructure practice

--- destructure.js
+++ destructure.js
@@ -1,76 +1,80 @@
-// let p = 5;
-// let q = 7;
+let p = 5;
+let q = 7;
 
-// [p, q] = [q, p];
-// console.log(p, q);
+[p, q] = [q, p];
+console.log(p, q);
 
-// let a, b, rest;
+let a, b, rest;
 
-// const x = [1, 2, 3, 4, 5];
+const x = [1, 2, 3, 4, 5];
 
-// [a, b, ...rest] = x;
+[a, b, ...rest] = x;
 
-// console.log(a, b, rest);
+console.log(a, b, rest);
 
-// const arr1 = [1, 2, 3, 4, 5];
-// const arr2 = [6, 7, 8, 9, 10];
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [6, 7, 8, 9, 10];
 
-// const c = [...arr1, ...arr2];
-// console.log(c);
+const c = [...arr1, ...arr2];
+console.log(c);
 
-// //parsing an array returned form a function
+//parsing an array returned form a function
 
-// function f(){
-//     return [1, 2, 3];
-// }
+function f(){
+    return [1, 2, 3];
+}
 
-// const [value,value2] = f();
-// console.log(value,value2);
+const [value,value2] = f();
+console.log(value,value2);
 
 
-// //ignoring some return values
-// const [value3, , value4] = f();
-// console.log(value3,value4);
+//ignoring some return values
+const [value3, , value4] = f();
+console.log(value3,value4);
 
-// //object destructuring 
-// const user = {
-//     name: 'anik sarker',
-//     id: 1,
-//     isVerified: true
-// };
+//object destructuring 
+const user = {
+    name: 'anik sarker',
+    id: 1,
+    isVerified: true
+};
 
-// const {id, isVerified, name} = user;
+const {id, isVerified, name} = user;
 
-// console.log(id,isVerified, name);
+console.log(id,isVerified, name);
 
-// //assign to new variable names 
+//assign to new variable names 
 
-// const o = {p: 42, q: true};
+const o = {p: 42, q: true};
 
-// const {p: foo, q: bar} = o;
+const {p: foo, q: bar} = o;
 
-// console.log(foo, bar);
+console.log(foo, bar);
 
-// const  {a = 10, b = 5} = {a: 3}
+const  {a = 10, b = 5} = {a: 3}
 
-// console.log(a,b);
+console.log(a,b);
 
-// const {a: aa = 10, b: bb = 20} = {a: 5};
-// console.log(aa,bb);
+const {a: aa = 10, b: bb = 20} = {a: 5};
+console.log(aa,bb);
 
 // unpacking fields from objects passed an function parameter
 const user = {
     id: 1,
     displayName: 'AS',
-    fullName = {
+    fullName: {
         firstName: 'Anik',
         lastName: 'Sarker'
     }
 };
 
-function userID({id}){
+function userID({ id }) {
     return id;
 }
 
-
+function whoIs({displayName, fullName:{firstName: fn, lastName: ln}}) {
+    return `${displayName} is ${fn} ${ln}`;
+}
+console.log(userID(user));
+console.log(whoIs(user));
 
---
+++ map.js
@@ -0,0 +1,8 @@
+const arr = [1,2,3,4,5];
+
+const arrCpy = arr.map(element => {
+    return element * 2;
+});
+
+console.log(arrCpy);
+
